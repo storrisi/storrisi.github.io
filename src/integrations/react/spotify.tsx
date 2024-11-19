d1a69ff4160159982bd7a17eebf662eb3848fdfa
Simone Torrisi <7654979+storrisi@users.noreply.github.com>
2024-11-19 23:34:07 +0100

    removed unused PlayerWrapper

--- src/integrations/react/spotify.tsx
+++ src/integrations/react/spotify.tsx
@@ -1,13 +1,7 @@
 /** @jsxImportSource react */
 
 import { qwikify$ } from "@builder.io/qwik-react";
-import styled from "@emotion/styled";
 import { Spotify } from "react-spotify-embed";
-
-const PlayerWrapper = styled("div")`
-  position: relative;
-  padding-top: 56.25%;
-`;
 
 export const PodcastPlayer = qwikify$(({ url }: { url: string }) => {
   console.log(url);
