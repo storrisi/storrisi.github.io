1c2ce27a84eb972d87efb9b1f1ebe8db675274a6
Simone Torrisi <7654979+storrisi@users.noreply.github.com>
2024-11-20 13:12:53 +0100

    update open graph preview

--- src/routes/layout-podcast.tsx
+++ src/routes/layout-podcast.tsx
@@ -1,6 +1,6 @@
 import { component$, Slot, useStyles$ } from "@builder.io/qwik";
 import { routeLoader$, useDocumentHead } from "@builder.io/qwik-city";
-import type { RequestHandler } from "@builder.io/qwik-city";
+import type { DocumentHead, RequestHandler } from "@builder.io/qwik-city";
 import { VideoPlayer } from "~/integrations/react/player";
 import { PodcastPlayer } from "~/integrations/react/spotify";
 import Header from "../components/starter/header/header";
@@ -8,40 +8,101 @@ import Footer from "../components/starter/footer/footer";
 
 import styles from "./styles.css?inline";
 
-export const onGet: RequestHandler = async ({ cacheControl }) => {
-  // Control caching for this request for best performance and to reduce hosting costs:
-  // https://qwik.builder.io/docs/caching/
-  cacheControl({
-    // Always serve a cached response by default, up to a week stale
-    staleWhileRevalidate: 60 * 60 * 24 * 7,
-    // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
-    maxAge: 5,
-  });
-};
-
-export const useServerTimeLoader = routeLoader$(() => {
-  return {
-    date: new Date().toISOString(),
-  };
-});
+import fs from "fs";
+import matter from "gray-matter";
+import path from "path";
 
 export default component$(() => {
   const { frontmatter, title } = useDocumentHead();
   useStyles$(styles);
-
-  console.log(title)
   return (
     <>
       <Header />
-        <h1 class="highlight"><span class="highlight">{title}</span></h1>
-        <div class="container container-center">
-          <Slot />
+      <h1 class="highlight">
+        <span class="highlight">{title}</span>
+      </h1>
+      <div class="container container-center">
+        <Slot />
+      </div>
+      <div class="container container-center container-flex episode-details">
+        <div>
+          <PodcastPlayer url={frontmatter.podcast_link} />
         </div>
-        <div class="container container-center container-flex episode-details">
-          <div><PodcastPlayer url={frontmatter.podcast_link} /></div>
-          <div><VideoPlayer video={frontmatter.youtube_id} /></div>
+        <div>
+          <VideoPlayer video={frontmatter.youtube_id} />
         </div>
+      </div>
       <Footer />
     </>
   );
 });
+
+export const useContents = routeLoader$(async (requestEvent) => {
+  const fileContents = fs.readFileSync(
+    path.join("src/routes/", requestEvent.url.pathname, "index@podcast.mdx")
+  );
+
+  const { data, content } = matter(fileContents);
+
+  return { ...data, content, url: requestEvent.url.href };
+});
+
+export const head: DocumentHead = ({ resolveValue, params }) => {
+  const { youtube_id, title, content, url }: any = resolveValue(useContents);
+
+  return {
+    title,
+    meta: [
+      {
+        name: "description",
+        content,
+      },
+      {
+        property: "og:image",
+        content: `http://img.youtube.com/vi/${youtube_id}/mqdefault.jpg`,
+      },
+      {
+        property: "twitter:image",
+        content: `http://img.youtube.com/vi/${youtube_id}/mqdefault.jpg`,
+      },
+      {
+        property: "twitter:card",
+        content: `summary_large_image`,
+      },
+      {
+        property: "og:title",
+        content: `${title} | TomorrowDevs Podcast`,
+      },
+      {
+        property: "twitter:title",
+        content: `${title} | TomorrowDevs Podcast`,
+      },
+      {
+        property: "og:description",
+        content    },
+      {
+        property: "twitter:description",
+        content      },
+      {
+        property: "og:image:width",
+        content: "1200",
+      },
+      {
+        property: "og:image:height",
+        content: "630",
+      },
+      {
+        property: "og:image:type",
+        content: "image/png",
+      },
+      {
+        property: "og:locale",
+        content: "it_IT",
+      },
+      {
+        property: "og:url",
+        content: url,
+      },
+    ],
+  };
+};
